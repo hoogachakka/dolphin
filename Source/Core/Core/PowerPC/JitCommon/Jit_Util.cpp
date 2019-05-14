95c1f14419b2a0ced79eafa2f9d9ffae547d4666
Tales Henrique Carvalho <tales-h-carvalho@hotmail.com>
2019-05-14 15:19:56 -0300

    Fixing Lua write memory functions and fixing memory leak in savestates.

--- Source/Core/Core/PowerPC/JitCommon/Jit_Util.cpp
+++ Source/Core/Core/PowerPC/JitCommon/Jit_Util.cpp
@@ -1080,3 +1080,10 @@ void EmuCodeBlock::JitClearCA()
 {
 	MOV(8, PPCSTATE(xer_ca), Imm8(0));
 }
+
+void EmuCodeBlock::Clear()
+{
+	registersInUseAtLoc.clear();
+	pcAtLoc.clear();
+	//exceptionHandlerAtLoc.clear();
+}
